18ee43e94ff690012fcdfe7ce81f1b4d2cd21bb6
Roman Hudz <hudzrom@gmail.com>
2025-01-09 22:37:39 +0200

    Troubles with removing

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,12 +1,14 @@
+import { readContacts } from '../utils/readContacts.js';
 import { writeContacts } from '../utils/writeContacts.js';
 
 export const removeAllContacts = async () => {
   try {
-    const emptyContacts = [];
-    await writeContacts(emptyContacts);
-    console.log('All contacts successfully removed!');
+    const data = await readContacts();
+    data.splice(0, data.length);
+    await writeContacts(data);
+    console.log('All contacts was deleted!!');
   } catch (err) {
-    console.log('Error removing contacts: ', err);
+    console.log('Error with deleting contacts: ', err);
   }
 };
 
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,9 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
 import { promises as fs } from 'node:fs';
+import { readContacts } from '../utils/readContacts.js';
 
-export const writeContacts = async (data) => {
+export const writeContacts = async (newContact) => {
   try {
-    const dataToWrite = JSON.stringify(data, null, 2);
+    const contacts = await readContacts();
+    if (newContact.length !== 0) {
+      contacts.push(newContact);
+    }
+    const dataToWrite = JSON.stringify(contacts, null, 2);
     await fs.writeFile(PATH_DB, dataToWrite, 'utf8');
     console.log('Contact successfully written!');
   } catch (err) {
